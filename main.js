07ed4a85dd62018eb3775bcfd6e4d1de872e0b39
kazuyamaru <82344948+kazuyamaru@users.noreply.github.com>
2025-10-26 17:31:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 $(window).scroll(function() {
     const scrollPercentage = ($(window).scrollTop() / ($(document).height() - $(window).height())) * 100;
-    if (scrollPercentage > 16) {
-      $('header').css('background-color', '#b0e0e6');
+    if (scrollPercentage > 5) {
+      $('header').css('background-color', 'white');
       $('header').css('transition', '0.3s');
-      $('header').css('z-index', '1000');
     } else {
-      $('header').css('background-color', 'transparent');
+      $('header').css('background-color', 'white');
     }
   });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const images = document.querySelectorAll(".about-image-box");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        images.forEach(img => img.classList.add("show"));
+        observer.unobserve(entry.target); // 一度だけ実行
+      }
+    });
+  }, { threshold: 0.3 }); // 30%見えたら発火
+
+  const aboutSection = document.querySelector("#about");
+  observer.observe(aboutSection);
+});
